168eeaa6ace370a9de7202a8b1836788873a6119
wortfor <orlov2007.zxc@gmail.com>
2026-09-07 10:09:28 +0700

    Решение задания 2 - класс Goods

---
+++ task_2.py
@@ -0,0 +1,11 @@
+class Goods:
+    title = "Мороженое"
+    weight = 154
+    tp = "Еда"
+    price = 1024
+
+# Изменяем price на 2048
+Goods.price = 2048
+
+# Добавляем новый атрибут inflation
+Goods.inflation = 100
